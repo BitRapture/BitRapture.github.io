d273807e4d28909e6019633bde2551da77505a11
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-11-15 05:59:31 +0000

    Start intersection functions

--- raycast.js
+++ raycast.js
@@ -7,18 +7,18 @@ const ctx = canvas.getContext("2d");
 // --------------
 // Functions
 // --------------
-// toDeg : input radians, output degrees
-//       : returns (degrees)
-function toDeg(radians) {
-  return radians * Math.PI / 180;
+// toDeg : input degrees, output radians
+//       : returns (radians)
+function toRad(degrees) {
+  return degrees * Math.PI / 180;
 }
 // checkMapVIntersect : returns distance of vertical intersection
 //                    : returns (intersection ? distance : -1)
 function checkMapVIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects) {
   let aY = (Math.floor(pY / mapHW) * mapHW) + (cAngle < 180 ? -1 : mapHW),
     aYM = (cAngle < 180 ? -mapHW : mapHW);
-  let aX = pX + ((pY - aY) / toDeg(Math.tan(cAngle))),
-    aXM = mapHW / toDeg(Math.tan(cAngle));
+  let aX = pX + (Math.abs(pY - aY) / Math.tan(toRad(cAngle))),
+    aXM = mapHW / Math.tan(toRad(cAngle));
 
   let i = 0, intersection = false;
   do {
@@ -48,8 +48,8 @@ function checkMapHIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects) {
   let d = (cAngle > 90 || cAngle < 270 ? -1 : 1);
   let aX = (Math.floor(pX / mapHW) * mapHW) + (d === 1 ? mapHW : d),
     aXM = mapHW * d;
-  let aY = pY + ((pX - aX) * toDeg(Math.tan(cAngle))),
-    aYM = mapHW * toDeg(Math.tan(cAngle));
+  let aY = pY + (Math.abs(pX - aX) * Math.tan(toRad(cAngle))),
+    aYM = mapHW * Math.tan(toRad(cAngle));
 
   let i = 0, intersection = false;
   do {
@@ -78,24 +78,26 @@ function checkMapHIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects) {
 function returnMapIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects) {
   let v = checkMapVIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects),
     h = checkMapHIntersect(map, mW, mH, mapHW, pX, pY, cAngle, maxIntersects);
-  return (v > 0 ? v : h);
+  return (v > 0 && v < h ? v : h);
 }
 
 // --------------
 // Game variables
 // --------------
-var playerAngle = 0, playerX = 128, playerY = 128,
-  playerFOV = 60;
+var playerAngle = 45, playerX = 130, playerY = 124,
+  playerFOV = 80;
 
 var cUnit = playerFOV / canvas.width,
-  cDistance = (canvas.width / 2) / toDeg(Math.tan(playerFOV / 2));
+  cDistance = (canvas.width / 2) / Math.tan(toRad(playerFOV / 2));
+
+console.log(cDistance);
 
 var mapWH = 64;
 var map = [
   1, 1, 1, 1, 1,
-  1, 0, 0, 1, 1,
   1, 0, 0, 0, 1,
-  1, 0, 1, 0, 1,
+  1, 0, 0, 0, 1,
+  1, 0, 0, 0, 1,
   1, 1, 1, 1, 1
 ];
 
@@ -107,19 +109,21 @@ function main(cTime) {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "red";
 
-  //playerAngle += 0.2;
-  //if (playerAngle > 360) playerAngle = 0;
+  playerAngle += 0.6;
+  if (playerAngle >= 360) playerAngle = 0;
 
   // Update raycast
   let cAngle = (playerAngle - (playerFOV / 2) < 0 ? 360 + (playerAngle - (playerFOV / 2)) : playerAngle - (playerFOV / 2));
-  for (let i = 0; i < canvas.width; ++i) {
-    let v = returnMapIntersect(map, 5, 5, mapWH, playerX, playerY, cAngle, 20);
-    console.log(v);
+  let i = 0;
+  do {
+    let v = returnMapIntersect(map, 5, 5, mapWH, playerX, playerY, cAngle, 4);
+    let tV = mapWH / v * 255;
+
+    ctx.fillRect(i, (canvas.height / 2) - (tV / 2), i + 1, tV);
 
     cAngle = (cAngle + cUnit > 360 ? (cAngle + cUnit) - 360 : cAngle + cUnit);
-  }
-
-  return 0;
+    i++;
+  } while (i < canvas.width);
 
   // Continue loop
   window.requestAnimationFrame(main);
