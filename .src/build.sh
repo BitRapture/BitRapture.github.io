c678a1a3096b98431f74256dd0f9b0719cdef1ba
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-14 22:24:17 +0000

    Setup for static page generation

---
+++ .src/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Get variables
+echo "Please provide the content file name"
+read V_CONTENT
+echo "Please enter the title name"
+read V_TITLE
+echo "Please enter the export file name"
+read V_EXPORT
+
+# Create new file and sandwich the content file between header.html and footer.html
+cat "templates/header.html" > "${V_EXPORT}.html"
+cat "page_content/${V_CONTENT}.html" | sed "s/__TITLE__/$V_TITLE/" >> "${V_EXPORT}.html"
+cat "templates/footer.html" >> "${V_EXPORT}.html"
+
+# Output success if no error is thrown
+echo "Done, created file: ${V_EXPORT}.html"
