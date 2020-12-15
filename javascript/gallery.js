b67e8e6e7d592a852b0b96ea065135562e950509
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-15 18:51:27 +0000

    Add gallery creation, add gallery css attributes

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,17 +1,37 @@
-var gallery = document.getElementById("gallery");
+var gallery;
 
+function previewContent(item) {
+  let index = gallerySrc.content.findIndex(i => i.name === item);
+  let view = document.getElementById("galleryView");
+  view.style.visibility = "visible";
+  view.innerHTML = "hi";
+}
 
 function loadContent() {
-  let previousNode = null;
-  gallerySrc.forEach(item => {
-    let galleryItem = document.createElement("div", {class : "item"});
-    let giImage = document.createElement("img", {src : item.image, alt : item.name});
-    let giCaption = document.createElement("div", {class : "caption"});
-    gallery.insertBefore()
+  gallerySrc.content.forEach(item => {
+    let galleryItem = document.createElement("div");
+    let giPreview = document.createElement("div");
+    let gipLink = document.createElement("a");
+    let giImage = document.createElement("img");
+    let giCaption = document.createElement("div");
+    galleryItem.className = "item";
+    giPreview.className = "preview";
+    gipLink.setAttribute("onclick", "previewContent('"+ item.name +"')")
+    gipLink.href = "#";
+    giImage.src = item.image;
+    giImage.alt = item.name;
+    giCaption.className = "caption";
+    giCaption.appendChild(document.createTextNode(item.name));
+    giPreview.appendChild(giCaption);
+    giPreview.appendChild(giImage);
+    gipLink.appendChild(giPreview);
+    galleryItem.appendChild(gipLink);
+    gallery.insertBefore(galleryItem, null);
   });
 }
 
 function main() {
+  gallery = document.getElementById("gallery");
   loadContent();
 }
 
