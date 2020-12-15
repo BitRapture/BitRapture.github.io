7911118c3b683e2c06ff4d09af691c31b9403dba
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-15 00:40:07 +0000

    Add ability to link optional files in header when building

--- .src/build.sh
+++ .src/build.sh
@@ -3,13 +3,21 @@
 # Get variables
 echo "Please provide the content file name"
 read V_CONTENT
+echo "Please provide an include file name (optional)"
+read V_INCLUDE
 echo "Please enter the title name"
 read V_TITLE
 echo "Please enter the export file name"
 read V_EXPORT
 
 # Create new file and sandwich the content file between header.html and footer.html
-cat "templates/header.html" | sed "s/__TITLE__/$V_TITLE/g" > "${V_EXPORT}.html"
+# Optional: add an include.html file that allows for css and js to be added to separate pages
+cat "templates/headerIncludes.html" > "${V_EXPORT}.html"
+if [ -a "page_includes/${V_INCLUDE}.html" ]
+then
+  cat "page_includes/${V_INCLUDE}.html" >> "${V_EXPORT}.html"
+fi
+cat "templates/header.html" | sed "s/__TITLE__/$V_TITLE/g" >> "${V_EXPORT}.html"
 cat "page_content/${V_CONTENT}.html" >> "${V_EXPORT}.html"
 cat "templates/footer.html" >> "${V_EXPORT}.html"
 mv "${V_EXPORT}.html" ".."
