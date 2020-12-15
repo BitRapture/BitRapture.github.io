e986ae2cde9958366c23b72bc29ba185c0794689
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-15 04:19:46 +0000

    Add typewriter effect, add description

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,31 @@
+var twContent = "", twI = 0,
+    twContentV = "", twContentPreserve = "";
+var typeWrite;
 
-function main() {
-
+function typeWriter(ID) {
+  document.getElementById(ID).innerHTML = twContentV;
+  twI++;
+  if (twI >= twContent.length) {
+    clearInterval(typeWrite);
+    document.getElementById(ID).innerHTML = twContentPreserve;
+  } else {
+    twContentV += twContent[twI];
+  }
 }
 
-window.onload = main();
+function startTypeWriter(ID) {
+  if (document.getElementById(ID).style.visibility === "visible") return;
+  twI = 0;
+  twContent = document.getElementById(ID).textContent;
+  twContentPreserve = document.getElementById(ID).innerHTML;
+  document.getElementById(ID).innerHTML = twContentV = twContent[twI];
+  document.getElementById(ID).style.visibility = "visible";
+  document.getElementById(ID).style.height = "auto";
+  typeWrite = setInterval(typeWriter, 50, ID);
+}
+
+function main() {
+  startTypeWriter("typeWriterDesc");
+}
+
+window.onload = main;
