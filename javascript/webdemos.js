b11a3780880adcb3b62b0f92a98bd89c7a48f1e8
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-15 18:52:26 +0000

    Add gallery test data, add gallery template to webdemos page

--- javascript/webdemos.js
+++ javascript/webdemos.js
@@ -1,6 +1,10 @@
 const gallerySrc = {
   content : [
-    { name : "2D Raycaster", desc : "",
-      image : "media/logo.jpg"  }
+    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
+    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
+    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
+    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
+    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
+    { name : "2D Bruh", desc : "", image : "media/logo.jpg"  }
   ]
 };
