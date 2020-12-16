52175d44a852eb156b6c211c46899acc1e5ee379
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-16 04:12:58 +0000

    Update to test modular gallery creation and links

--- javascript/webdemos.js
+++ javascript/webdemos.js
@@ -1,10 +1,8 @@
 const gallerySrc = {
   content : [
-    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
-    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
-    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
-    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
-    { name : "2D Raycaster", desc : "", image : "media/logo.jpg"  },
-    { name : "2D Bruh", desc : "", image : "media/logo.jpg"  }
+    { name : "Canvas Raycaster", desc : "A raycasting engine made using the HTML canvas and JavaScript. Move by using the arrow keys.",
+    image : "media/webdemos/raycaster.png", link : "raycasting.html", linkdesc : "Try it out"  },
+    { name : "Image Slideshow", desc : "", image : "media/logo.jpg"  },
+    { name : "Bullet Hell Game", desc : "", image : "media/logo.jpg"  }
   ]
 };
