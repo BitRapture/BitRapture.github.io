ef726bf98f4931fd6b265f8c44fc3aa80848b168
BitRapture <thinzy.business+bitrapture@gmail.com>
2020-12-18 09:05:46 +0000

    Add comments

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,9 +1,22 @@
+// --------------
+// Gallery context
+// --------------
 var gallery;
 
+// --------------
+// Functions
+// --------------
+
+// previewContent : When an item is clicked, this function renders and displays
+//                : The preview pane.
 function previewContent(item) {
+  // Find the item that was clicked on via its name
   let galleryItem = gallerySrc.content[gallerySrc.content.findIndex(i => i.name === item)];
+  // Get the preview pane context
   let view = document.getElementById("galleryView");
+  // Set the preview pane to nothing in case another item is clicked
   view.innerHTML = "";
+  // Get all items and blur them, set the preview pane to be visible
   if (view.style.visibility !== "visible" || view.style.visibility === "") {
     let items = document.getElementById("gallery").getElementsByClassName("item");
     for (let i = 0; i < items.length; ++i) {
@@ -11,6 +24,7 @@ function previewContent(item) {
     }
     view.style.visibility = "visible";
   }
+  // Declare and define all new elements to be added to the preview pane
   let viewImage = document.createElement("img");
   let imagePane = document.createElement("div");
   let infoPane = document.createElement("div");
@@ -22,6 +36,7 @@ function previewContent(item) {
   let buttonButton = document.createElement("div");
   let buttonLink = document.createElement("a");
 
+  // Set the styles and attributes accordingly
   infoPane.style.width = "40%";
   infoPane.style.minHeight = "300px";
   imagePane.style.width = "60%";
@@ -35,6 +50,7 @@ function previewContent(item) {
   buttonButton.id = "button";
   buttonLink.href = galleryItem.link;
 
+  // Append the elements inside of each other, and then finally to the preview pane
   buttonLink.appendChild(document.createTextNode("["+ galleryItem.linkdesc +"]"));
   closeLink.appendChild(document.createTextNode("[close]"));
   closeButton.appendChild(closeLink);
@@ -53,24 +69,35 @@ function previewContent(item) {
   view.appendChild(infoPane);
 }
 
+// closePreview : When the close button is pressed it closes down the preview
+//              : pane.
 function closePreview() {
+  // Get preview and items context
   let view = document.getElementById("galleryView");
   let items = document.getElementById("gallery").getElementsByClassName("item");
+  // Reset the preview pane to nothing and hide it
   view.innerHTML = "";
   view.style.visibility = "hidden";
+  // Unblur all the items
   for (let i = 0; i < items.length; ++i) {
     items[i].style.filter = "";
   }
 }
 
+// loadContent : Load the content from the gallerySrc variable defined outside
+//             : of main JavaScript so it can be implemented in more than one
+//             : webpage. Render flexbox containers to the page.
 function loadContent() {
+  // For each item in the array, create a container
   gallerySrc.content.forEach(item => {
+    // Declare and define all new elements to be added to the item container
     let galleryItem = document.createElement("div");
     let giPreview = document.createElement("div");
     let gipLink = document.createElement("a");
     let giImage = document.createElement("img");
     let giCaption = document.createElement("div");
 
+    // Set the styles and attributes accordingly
     galleryItem.className = "item";
     giPreview.className = "preview";
     gipLink.setAttribute("onclick", "previewContent('"+ item.name +"')");
@@ -79,6 +106,7 @@ function loadContent() {
     giImage.alt = item.name;
     giCaption.className = "caption";
 
+    // Append the elements inside of each other, and then finally to the item container
     giCaption.appendChild(document.createTextNode(item.name));
     giPreview.appendChild(giCaption);
     giPreview.appendChild(giImage);
@@ -88,9 +116,13 @@ function loadContent() {
   });
 }
 
+// This function is ran once the page loads
 function main() {
+  // Set global gallery context
   gallery = document.getElementById("gallery");
+  // Load and render the items
   loadContent();
 }
 
+// Load into main function once webpage loads
 window.onload = main;
--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,19 @@
+// --------------
+// Constants
+// --------------
 const colorA = "black";
 const colorB = "white";
 
+// toggleNavList : This toggles the navigation menu on the webpage
 function toggleNavList() {
+  // Get element contexts
   let root = document.documentElement;
   let rootColor = root.style.getPropertyValue("--primary-color");
   let nav = document.getElementById("navList");
   let navB = document.getElementById("navButton");
+
+  // If the navigation menu is invisible, display it and give it a size so that
+  // page content scales
   if (nav.style.visibility === "hidden" || nav.style.visibility === "") {
     nav.style.visibility = "visible";
     nav.style.width = "200px";
@@ -21,23 +29,30 @@ function toggleNavList() {
   }
 }
 
+// toggleTheme : Toggles the theme between dark mode and light mode
 function toggleTheme() {
+  // Get element contexts
   let root = document.documentElement;
   let nav = document.getElementById("navList");
   let navB = document.getElementById("navButton");
   let themeB = document.getElementById("themeButton");
+
+  // Reverse the colourscheme
   let toggleB = (root.style.getPropertyValue("--primary-color") === colorB ? colorA : colorB);
   let toggleF = (toggleB === colorB ? colorA : colorB);
 
+  // Invert the declared CSS variables from their current colour
   root.style.setProperty("--primary-color", toggleB);
   root.style.setProperty("--secondary-color", toggleF);
 
+  // Change the navigation menu button to the correct theme verison
   if (nav.style.visibility === "hidden" || nav.style.visibility === "") {
     navB.src = "media/buttons/navListButtonOpen"+ (toggleB === colorA ? "" : "Light") +".png";
   } else {
     navB.src = "media/buttons/navListButtonClose"+ (toggleB === colorA ? "" : "Light") +".png";
   }
 
+  // Change the theme button to the correct theme verison
   if (toggleB === colorA) {
     themeB.src = "media/buttons/themeButtonOff.png";
   } else {
